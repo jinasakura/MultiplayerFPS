e0069dc8a94f0e2c655fa017ace636a044929932
jinasakura <jinasakura@qq.com>
2016-09-08 18:25:41 +0800

    增加了加入房间的功能，可是不知道为什么现在连创建房间后，房间都找不到了

---
+++ Assets/Scripts/JoinGame.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+using System;
+
+public class JoinGame : MonoBehaviour
+{
+
+    List<GameObject> roomList = new List<GameObject>();
+
+    private NetworkManager networkManager;
+
+    [SerializeField]
+    private Text status;
+
+    [SerializeField]
+    private GameObject roomListItemPrefab;
+
+    [SerializeField]
+    private Transform roomListParent;
+
+    // Use this for initialization
+    void Start()
+    {
+        networkManager = NetworkManager.singleton;
+        if (networkManager.matchMaker == null)
+        {
+            networkManager.StartMatchMaker();
+        }
+
+        RefreshRoomList();
+    }
+
+    public void RefreshRoomList()
+    {
+        networkManager.matchMaker.ListMatches(0, 20, "", false, 0, 0, OnMatchList);
+        status.text = "Loading...";
+    }
+
+    private void OnMatchList(bool success, string extendedInfo, List<MatchInfoSnapshot> responseData)
+    {
+        //throw new NotImplementedException();
+        status.text = "";
+        if (!success)
+        {
+            status.text = "Couldn't get Room List";
+            return;
+        }
+
+        ClearRoomList();
+        foreach (MatchInfoSnapshot item in responseData)
+        {
+            GameObject _roomListItemGO = Instantiate(roomListItemPrefab);
+            _roomListItemGO.transform.SetParent(roomListParent);
+
+            RoomListItem _roomListItem = _roomListItemGO.GetComponent<RoomListItem>();
+            if (_roomListItem != null)
+            {
+                _roomListItem.Setup(item, JoinRoom);
+            }
+            //that will take care of setting up the name/amount of user
+            //as well as setting up a callback function that will join the game.
+
+            roomList.Add(_roomListItemGO);
+        }
+
+        if (roomList.Count == 0)
+        {
+            status.text = "No room at the moment.";
+        }
+    }
+
+
+    void ClearRoomList()
+    {
+        for (int i = 0; i < roomList.Count; i++)
+        {
+            Destroy(roomList[i]);
+        }
+        roomList.Clear();
+    }
+
+    public void JoinRoom(MatchInfoSnapshot _match)
+    {
+        Debug.Log("Joining" + _match.name);
+        //networkManager.matchMaker.JoinMatch(_match.networkId, "", "", "", 0, 0, networkManager.OnMatchJoined);
+        //ClearRoomList();
+        //status.text = "JOINING....";
+    }
+
+}
